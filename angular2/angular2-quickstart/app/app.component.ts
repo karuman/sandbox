6bb7d5e5c7120682020fe841dadc5bbd62a85bf9
Namura Ken <jl26165@jp.ibm.com>
2016-02-07 09:03:09 +0900

    input parameters are added

--- angular2/angular2-quickstart/app/app.component.ts
+++ angular2/angular2-quickstart/app/app.component.ts
@@ -1,14 +1,15 @@
 import {Component} from 'angular2/core';
 import {ChildComponent} from './components/child.component';
+import {InjChildComponent} from './components/injchild.component';
 
 @Component({
     selector: 'my-app',
     template: `
 <h1>My First Angular 2 App</h1>
 <my-child>Loading...</my-child>
+<my-injchild>Loading...</my-injchild>
 `,
-directives: [ChildComponent]
+directives: [ChildComponent,InjChildComponent]
 })
 export class AppComponent {
-
 }
