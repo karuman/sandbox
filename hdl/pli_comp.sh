8802a158dc980d9b6917c3795f9b3b435e244ee8
ken namura <karuman@gmail.com>
2016-12-10 12:34:37 +0900

    PLI sample

---
+++ hdl/pli_comp.sh
@@ -0,0 +1,8 @@
+#
+gcc  -c -g  -fPIC -m32 -I$MTI_DIR/include  my_pli.c 
+gcc -m32 -shared -Bsymbolic -L$MTI_DIR/linuxaloem -lmtipli -o my_pli.sl my_pli.o
+
+vlog test_module.v 
+
+vsim test_module -pli my_pli.sl -batch
+
