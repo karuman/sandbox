614bc23a32887ee2a4706750165f1d65541f081a
ken namura <karuman@gmail.com>
2016-12-17 14:44:05 +0900

    bitをパラメータで指定。ファイルへ出力

--- wt/gen_odg.py
+++ wt/gen_odg.py
@@ -1,8 +1,27 @@
 #
 #
 
-a=13
-b=13
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("a", type=int, help="A bit width")
+parser.add_argument("b", type=int, help="B bit width")
+parser.add_argument('-f', '--file_name', \
+        action='store', \
+        nargs='?', \
+        const=None, \
+        default=None, \
+        type=str, \
+        choices=None, \
+        help='output file name', \
+        metavar=None)
+args=parser.parse_args()
+
+
+a=args.a
+b=args.b
+fo_obj = open('test.odg', 'w')
+fh_obj = open('test.hdl', 'w')
 
 for i in range(1,a+b):
     c=min(i,a)
@@ -10,6 +29,7 @@ for i in range(1,a+b):
     for j in range(0,c):
         if((i-j)<b+1):
             s=s+'o'
+            print >> fh_obj,'wire a'+str(j)+'b'+str(i-1) +'=a['+str(j)+'] & b['+str(i-1-j)+'];'
         else:
             s=s+' '
-    print s
+    print >> fo_obj, s
