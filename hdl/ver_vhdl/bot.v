caa60492e1073f2bb18e496cfb9b3b3221de4ada
ken namura <karuman@preferred.jp>
2017-04-24 10:02:29 +0900

    add parameter in verilog

--- hdl/ver_vhdl/bot.v
+++ hdl/ver_vhdl/bot.v
@@ -1,4 +1,6 @@
 module bot(bot_p1);
    input bot_p1;
-   initial $monitor($stime ,, "bot_p1 = %b" , bot_p1);
+   parameter PullTime = 10000;
+
+   initial $monitor($stime ,, "bot_p1 = %b %s" , bot_p1,PullTime);
 endmodule
--- hdl/ver_vhdl/comp.sh
+++ hdl/ver_vhdl/comp.sh
@@ -1,6 +1,7 @@
 #
 ncvhdl -v93 test.vhd 
 ncvlog bot.v
-ncelab mid1 -access +rwc
+ncelab mid1 -access +rwc -gpg "mid1.bot_inst.PullTime=>102"
+
 ncsim mid1 -batch
 
